70c832f9858d38e766321b18487bcc926e2659e6
NureAlam68 <nurealam151068@gmail.com>
2025-02-14 12:45:04 +0600

    deploy

--- src/app/dashboard/products/add/components/ProductAddForm.jsx
+++ src/app/dashboard/products/add/components/ProductAddForm.jsx
@@ -1,34 +1,44 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const ProductAddForm = () => {
-    const router = useRouter();
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        const form = e.target;
-        const name = form.productName.value;
-        const payload = { name };
-        const res = await fetch('http://localhost:3000/api/items', {
-            method: 'POST',
-            body: JSON.stringify(payload),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        })
-        const result = await res.json();
-        form.reset();
-        // alert('Product Added')
-        router.push("/products")
-    }
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.productName.value;
+    const payload = { name };
+    const res = await fetch(
+      "https://core-concept-nextjs-sigma.vercel.app/api/items",
+      {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const result = await res.json();
+    form.reset();
+    // alert('Product Added')
+    router.push("/products");
+  };
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" name='productName' placeholder='Product name' className='border p-1 mr-2 rounded-md'/>
-        <button type='submit' className='px-2 p-1 bg-green-500 rounded-md'>Submit</button>
+        <input
+          type="text"
+          name="productName"
+          placeholder="Product name"
+          className="border p-1 mr-2 rounded-md"
+        />
+        <button type="submit" className="px-2 p-1 bg-green-500 rounded-md">
+          Submit
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ProductAddForm
+export default ProductAddForm;
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,22 +1,29 @@
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { redirect } from "next/navigation";
+import React from "react";
 
-const ProductsPage = async() => {
-    const res = await fetch('http://localhost:3000/api/items', {
-        cache: 'force-cache'
-    })
-    const data = await res.json()
+export const dynamic = "force-dynamic";
 
-    if(data.length < 20) {
-        redirect("/dashboard/products/add")
-    }
+const ProductsPage = async () => {
+  // const res = await fetch('https://core-concept-nextjs-sigma.vercel.app/api/items', {
+  //     cache: 'force-cache'
+  // })
+  const res = await fetch(
+    "https://core-concept-nextjs-sigma.vercel.app/api/items"
+  );
+  const data = await res.json();
+
+  // if(data.length < 20) {
+  //     redirect("/dashboard/products/add")
+  // }
   return (
-    <div className='mt-10 px-20 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-      {
-        data?.map(item => <h1 className='border p-4 bg-green-300' key={item._id}>{item.name}</h1>)
-      }
+    <div className="mt-10 px-20 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+      {data?.map((item) => (
+        <h1 className="border p-4 bg-green-300" key={item._id}>
+          {item.name}
+        </h1>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
